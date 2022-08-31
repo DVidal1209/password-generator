f7195cbbc21fc38543b990c81a468d065b565bac
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-08-31 13:29:26 -0400

    Relinked stylesheet and javascript based on differing branch structure

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", "\u005c", ";", ":", "'", "\u0022", "?", "<", ".", ",", ">", "/"];
-
-console.log(upperCase);
+var totalCharacters = [];
+console.log(totalCharacters);
 
 // // Assignment Code
 // var generateBtn = document.querySelector("#generate");
